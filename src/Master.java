5964781ed9a87b573dc64c057cd9b0169b0c4ad7
shivamka1 <4599890+shivamka1@users.noreply.github.com>
2025-12-01 00:31:17 +0000

    impl master watcher

--- src/Master.java
+++ src/Master.java
@@ -1,9 +1,31 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
 
 public class Master implements Watcher {
+
+    String connectString;
+    ZooKeeper zk;
+
+    Master(String connectString) {
+        this.connectString = connectString;
+    }
+
+    void startZk() throws IOException {
+        zk = new ZooKeeper(connectString, 15000, this);
+    }
+
     @Override
     public void process(WatchedEvent watchedEvent) {
+        System.out.println(watchedEvent);
+    }
 
+    static void main(String[] args) throws Exception {
+        Master master = new Master(args[0]);
+        master.startZk();
+
+        Thread.sleep(60000);
     }
 }
